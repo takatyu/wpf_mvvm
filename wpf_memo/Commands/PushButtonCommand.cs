2145d2b294256ed40905922ed31d5f3e862e5fd8
tkmini <tkmini@TKMINI3AE4>
2021-07-22 16:42:04 +0900

    プロジェクト ファイルを追加します。

---
+++ wpf_memo/Commands/PushButtonCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace wpf_memo.Commands
+{
+    class PushButtonCommand : ICommand
+    {
+
+        private readonly ViewModels.MainWindowViewModel _viewModel;
+
+        public PushButtonCommand(ViewModels.MainWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        /*
+         * 継承の実装
+         * */
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+            // throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.Count++;
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ wpf_memo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using wpf_memo.Commands;
+
+namespace wpf_memo.ViewModels
+{
+    /*
+     * ViewModel
+     * ViewとModelの間を取り持つクラス
+     */
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+
+            set
+            {
+                _count = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Count"));
+                }
+            }
+
+        }
+
+        public ICommand PushButtonCommand
+        {
+            get
+            {
+                if (_pushButtonCommnad == null)
+                {
+                    _pushButtonCommnad = new PushButtonCommand(this);
+                }
+
+                return _pushButtonCommnad;
+            }
+        }
+
+        private int _count;
+
+        private ICommand _pushButtonCommnad;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
